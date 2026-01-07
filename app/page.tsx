3776d204708b57a211f3f42bdb67b971bb1ed564
daeun <qzsz797979@gmail.com>
2026-01-07 23:20:36 +0900

    refactor: 초기 css 등 불필요한 코드 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,3 @@
-import { auth } from "@/auth";
-import { LoginButton } from "./_components/LoginButton";
-
 export default async function Page() {
-  const session = await auth();
-
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>Simple Planner</h1>
-      <LoginButton isAuthed={!!session} />
-      <pre style={{ marginTop: 16 }}>{JSON.stringify(session, null, 2)}</pre>
-    </main>
-  );
+  return <>main page</>;
 }
